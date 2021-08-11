0280b2ed2fb600492126575e4528e6a505d78b6c
Rhys Malyon <malyonr@gmail.com>
2021-08-11 11:46:05 +0900

    Add restaurant and review routes

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,12 +3,3 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true, numericality: true, inclusion: 0..5
 end
-
-# Validation
-
-#     A restaurant must have a name, an address and a category.
-#     A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
-#     When a restaurant is destroyed, all of its reviews must be destroyed as well.
-#     A review must belong to a restaurant.
-#     A review must have content and a rating.
-#     A review’s rating must be a number between 0 and 5.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index create new show] do
+    resources :reviews, only: %i[create new]
+  end
 end
